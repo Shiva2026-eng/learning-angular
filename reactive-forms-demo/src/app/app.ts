8ac1526c34d600bcbc3a5c16111f12ba6ea6d19c
Shiva2026-eng <Shivansh.Lavaniya@veersatech.com>
2026-07-12 21:23:39 +0530

    Learnt about FormControl and created a basic input using it

--- reactive-forms-demo/src/app/app.ts
+++ reactive-forms-demo/src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import {ReactiveFormsModule,FormControl} from '@angular/forms'
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('reactive-forms-demo');
+  name=new FormControl('Shivansh lavaniya')
+  constructor(){
+    console.log(this.name.value);
+  }
 }
