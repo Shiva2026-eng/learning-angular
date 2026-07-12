bf946889376b63e2cd9015008e41a450c33a86d5
Shiva2026-eng <Shivansh.Lavaniya@veersatech.com>
2026-07-12 15:34:59 +0530

    created home,about and contact pages along with the student-list components

---
+++ student-management-system/src/app/students/students-list/students-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-students-list',
+  standalone: false,
+  templateUrl: './students-list.html',
+  styleUrl: './students-list.scss',
+})
+export class StudentsList {}
