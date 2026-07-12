b06d63cdb681eb33b7e09cb57f56c856dab836b4
Shiva2026-eng <Shivansh.Lavaniya@veersatech.com>
2026-07-12 16:41:41 +0530

    Created Routes

--- student-management-system/src/app/app-routing-module.ts
+++ student-management-system/src/app/app-routing-module.ts
@@ -1,10 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { Home } from './home/home';
+import { About } from './about/about';
+import { Contact } from './contact/contact';
+import { StudentsList } from './students/students-list/students-list';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: Home
+  },
+  {
+    path: 'students',
+    component: StudentsList
+  },
+  {
+    path: 'about',
+    component: About
+  },
+  {
+    path: 'contact',
+    component: Contact
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    Home,
+    About,
+    Contact,
+    StudentsList
+  ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
