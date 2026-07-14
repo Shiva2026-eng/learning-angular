2501f511f7bcdec366a31b1d314eda44d0bfc40e
Shiva2026-eng <Shivansh.Lavaniya@veersatech.com>
2026-07-14 11:26:51 +0530

    Created routes

--- products-catalogue/src/app/app.routes.ts
+++ products-catalogue/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { Home } from './home/home';
+import { About } from './about/about';
+import { Categories } from './categories/categories';
+export const routes: Routes = [
+  { path: '', component: Home },
+  {
+    path: 'about',
+    component: About,
+  },
+  {
+    path: 'categories',
+    component: Categories,
+  },
+];
