59a166bfdc8e7d9532b57cc7306dfc545c49c30f
Shiva2026-eng <Shivansh.Lavaniya@veersatech.com>
2026-07-14 11:45:50 +0530

    Imported product_service in the home component and called the api to get all products

--- products-catalogue/src/app/home/home.ts
+++ products-catalogue/src/app/home/home.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { Productservice } from '../services/productservice';
 @Component({
   selector: 'app-home',
   imports: [],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {}
+export class Home implements OnInit {
+  private product_service = inject(Productservice);
+  products = signal<any[]>([]);
+  ngOnInit(): void {
+    this.product_service.getUsers().subscribe((response: any) => {
+      this.products.set(response.products);
+      console.log(this.products());
+    });
+  }
+}
